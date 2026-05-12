0960151c5824090b4986e84ec775a0e8f318e22e
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2026-05-12 14:09:24 -0600

    Actualizar estilos de texto y agregar colores de marca en la configuración de Tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -3,23 +3,27 @@ import { Text, View } from 'react-native';
 import './global.css';
 
 const App = () => {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
+   return (
+    <View className="flex-1 items-center justify-center bg-white px-10">
       
-      {/* Usando la variante Black */}
-      <Text className="text-4xl font-work-black text-blue-800">
-        Hola Mundo Black
+      <Text className="text-5xl font-work-black text-primary">
+        Primary
       </Text>
 
-      {/* Usando la variante Medium */}
-      <Text className="text-3xl font-work-medium text-gray-800 mt-4">
-        Hola Mundo Medium
+      <Text className="text-4xl font-work-medium text-secondary mt-4">
+        Secondary
       </Text>
 
-      {/* Usando la variante Light */}
-      <Text className="text-2xl font-work-light text-gray-500 mt-4">
-        Hola Mundo Light
+      <Text className="text-3xl font-work-light text-tertiary mt-4">
+        Tertiary
       </Text>
+
+      {/* Ejemplo de un botón usando bg-primary */}
+      <View className="mt-10 bg-primary p-4 rounded-2xl w-full items-center">
+        <Text className="text-white font-work-medium text-lg">
+          Botón de Marca
+        </Text>
+      </View>
 
     </View>
   );
