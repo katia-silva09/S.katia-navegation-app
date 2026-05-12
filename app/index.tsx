fddf0f812f77bb3cad13b123e74f956f23874e58
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2026-05-12 14:12:56 -0600

    Actualizar la estructura de navegación en el componente App

--- app/index.tsx
+++ app/index.tsx
@@ -1,32 +1,36 @@
 import { Text, View } from 'react-native';
 // IMPORTANTE: Importar el CSS global
+import { Link } from 'expo-router'; // Importamos el componente Link
 import './global.css';
 
 const App = () => {
-   return (
-    <View className="flex-1 items-center justify-center bg-white px-10">
-      
-      <Text className="text-5xl font-work-black text-primary">
-        Primary
+return (
+    <View className="flex-1 px-10 py-20">
+      <Text className="text-4xl font-work-black text-primary mb-10">
+        Menú Principal
       </Text>
 
-      <Text className="text-4xl font-work-medium text-secondary mt-4">
-        Secondary
-      </Text>
-
-      <Text className="text-3xl font-work-light text-tertiary mt-4">
-        Tertiary
-      </Text>
-
-      {/* Ejemplo de un botón usando bg-primary */}
-      <View className="mt-10 bg-primary p-4 rounded-2xl w-full items-center">
-        <Text className="text-white font-work-medium text-lg">
-          Botón de Marca
+      {/* Navegación a Productos */}
+      <Link href="/products" className="mb-5">
+        <Text className="text-xl font-work-medium text-secondary">
+          Ir a Productos
         </Text>
-      </View>
+      </Link>
 
+      {/* Navegación a Perfil */}
+      <Link href="/profile" className="mb-5">
+        <Text className="text-xl font-work-medium text-secondary">
+          Ir a Perfil
+        </Text>
+      </Link>
+
+      {/* Navegación a Ajustes */}
+      <Link href="/settings">
+        <Text className="text-xl font-work-medium text-secondary">
+          Ir a Ajustes
+        </Text>
+      </Link>
     </View>
-  );
-};
+  );};
 
 export default App;
